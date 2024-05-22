4f9d2f8be40f5725105f31142f3a5011f6e9cd8e
daleparker <daleparkerwinston@gmail.com>
2024-05-22 08:34:16 -0700

    removed unused import

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.apache.commons.logging.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
